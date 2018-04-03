dedf7b08205690cf6e381e9a99119b3c223152d7
Juvid <jutewei@qq.com>
2018-04-03 17:24:07 +0800

    系统默认播放

--- JuAVPlayer/ViewController.m
+++ JuAVPlayer/ViewController.m
@@ -13,6 +13,7 @@
     __weak IBOutlet JuAVPlayer *ju_vieVideo;
     __weak IBOutlet UIProgressView *ju_progressView;
     __weak IBOutlet UIButton *ju_btnPlay;
+    AVPlayerViewController *_playerVc;
 }
 @end
 
@@ -22,7 +23,22 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self juSetPlayerView];
+    [self juSetAVPlay];
     // Do any additional setup after loading the view.
+}
+-(void)juSetAVPlay{
+    NSURL *remoteUrl = [NSURL URLWithString:@"http://w2.dwstatic.com/1/5/1525/127352-100-1434554639.mp4"];
+
+    _playerVc = [[AVPlayerViewController alloc] init];
+    _playerVc.player =[AVPlayer playerWithURL:remoteUrl];
+    _playerVc.videoGravity = AVLayerVideoGravityResize;
+    _playerVc.view.translatesAutoresizingMaskIntoConstraints = YES;
+    _playerVc.view.frame = CGRectMake(0, 260, 414, 200);
+    [self.view addSubview:_playerVc.view];
+
+    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:remoteUrl]; // create item
+    [_playerVc.player   replaceCurrentItemWithPlayerItem:playerItem]; // replaceCurrentItem
+    [_playerVc.player play];
 }
 /**
  创建播放器视图
